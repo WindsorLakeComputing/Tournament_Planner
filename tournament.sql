7674965f21dc561571f771a09532bd5240398936
bjk1456 <Benjamin.J.Kelly@live.com>
2015-02-25 10:13:35 -0500

    Update tournament.sql
    Changed data types of winner and loser inside of match.

--- tournament.sql
+++ tournament.sql
@@ -3,6 +3,6 @@ CREATE TABLE player ( content NAME,
                      time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                      id SERIAL );
 
-CREATE TABLE match ( WINNER character(1),
-                     LOSER character(1),
+CREATE TABLE match ( winner INTEGER,
+                     loser INTEGER,
 		     id SERIAL );
