373a7897e232ea4ac78c46e891eb016824f25848
bjk1456 <Benjamin.J.Kelly@live.com>
2015-03-08 23:52:18 -0400

    Update tournament.sql
    Added playerStandings()

--- tournament.sql
+++ tournament.sql
@@ -32,3 +32,21 @@ from
 ) a
 GROUP BY player
 ORDER BY player desc;
+
+CREATE OR REPLACE FUNCTION playerStandings() 
+         RETURNS TABLE (player integer, player_name name, wins bigint, total_matches bigint) AS $$
+         BEGIN
+         IF (SELECT count(*) FROM match) > 1 THEN
+           	RETURN QUERY SELECT tm.player, p.player_name, coalesce(m.wins, 0), tm.total_matches
+           	FROM ( 
+                	SELECT winner, count(*) as wins
+                	FROM match 
+                	GROUP BY match.winner 
+                	) m RIGHT JOIN view_total_matches tm ON m.winner = tm.player 
+                JOIN player p on tm.player = p.id
+                ORDER BY m.wins DESC NULLs LAST; 
+        ELSE 
+                RETURN QUERY SELECT player.id, player.player_name, CAST(0 AS BIGINT), CAST(0 AS BIGINT)  FROM player; 
+        END IF; 
+END
+$$ LANGUAGE plpgsql; 
